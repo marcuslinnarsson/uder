93cb3d9bfe05a71d764b2e9790ab7f02e657e22e
marcuslinnarsson <marcus.linnarsson8@gmail.com>
2020-07-04 22:31:37 +0200

    removed

--- uder/ViewController.swift
+++ uder/ViewController.swift
@@ -14,9 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    override func viewWillAppear(_ animated: Bool) {
-        <#code#>
-    }
+
 
 }
 
